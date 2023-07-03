436865a226b00755f0907ae5e6eed6f439d42b6f
lakki5777 <137788317+lakki5777@users.noreply.github.com>
2023-07-03 17:28:58 +0530

    added media

--- components/Product Overview/index.tsx
+++ components/Product Overview/index.tsx
@@ -22,7 +22,7 @@ import DemoForm from '../Demo Form';
 const ProductOverview = () => {
   return (
     <div className="mt-[80px]">
-      <div className="bg-[url('/images/bg-blue2.png')] bg-no-repeat bg-cover md:px-[144px] py-[75px]">
+      <div className="bg-[url('/images/bg-blue2.png')] bg-no-repeat bg-cover md:px-[134px] py-[75px]">
         <div className="max-w-[1377.5px] mx-auto">
           <Button
             label={"PRODUCT OVERVIEW"}
@@ -30,15 +30,15 @@ const ProductOverview = () => {
             // onClick={() => router.push("/getStarted")}
             className="max-w-[150px] px-[10px] py-[4px] bg-[#A1B4F7] text-[white] xxl:leading-[22.99px] tracking-[-0.25px] font-semibold sm:leading-[20.57px] leading-[14px] xxl:w-[238px] md:w-[200px] w-[160px] text-[12px]"
           />
-          <div className="md:my-[30px]  flex md:flex-row flex-col">
+          <div className="md:my-[30px]  lg:gap-[40px] flex md:flex-row flex-col">
           <div className='max-w-[336px] inline-block text-[white] md:text-[60px] text-[30px] leading-[64px] tracking-[-1.8px] font-bold md:mt-[30px] mt-[15px]'>
             Build Exclusively
           </div>
-          <div className="max-w-[742px] inline-block text-[white] md:mt-[62px] md:text-[22px] text-[16px] font-medium leading-[35px]">
+          <div className="max-w-[704px] inline-block text-[white] md:mt-[62px] md:text-[22px] text-[16px] font-medium leading-[35px]">
             Appinventiv dream, architect, design and develop. But more than that, we are a team of passionate people driven by one relentless pursuit- to craft innovative solutions and deliver unparalleled results.
           </div>
         </div>
-        <div className='flex flex-row pb-[50px] justify-between'>
+        <div className='flex lg:pr-[125px] flex-row pb-[50px] justify-between'>
           <div>
             <div className='max-w-[336px] inline-block text-[white] md:text-[60px] text-[30px] leading-[86px] tracking-[-2.4px] font-bold mt-[30px]'>
               250+

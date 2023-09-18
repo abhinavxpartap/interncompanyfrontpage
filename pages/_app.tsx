2e251f2f45196fc66bc9e72e3d55841e3112ef0b
Lucky <lucky@alumeltech.com>
2023-09-18 11:50:09 +0530

    new addition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import {LoaderContext, LoaderProvider} from "../context/LoaderContext";
+import {useContext} from "react";
+import Loader from "../utils/Loader";
+import {useRouter} from "next/router";
+import {AuthProvider} from "../context/AuthContext";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const {isLoading, setIsLoading} = useContext(LoaderContext);
+
+  const router = useRouter();
+  const unprotectedRoutes = [
+    "/",
+    "/aboutus",
+    "/blog",
+    "/blogDetailVeiw",
+    "/contact",
+    "/industries",
+    "/portfolio",
+    "/portfolio2",
+    "/service",
+    "/login",
+    "/admin",
+    "/finance",
+    "/realestate",
+    "/mobileApp",
+    "/automobile"
+
+  ];
+
+  return <LoaderContext.Provider value={{isLoading, setIsLoading}}>
+    <LoaderProvider>
+      {
+        !unprotectedRoutes.includes(router.pathname) ? <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider> : <Component {...pageProps} />
+      }
+      <Loader/>
+    </LoaderProvider>
+  </LoaderContext.Provider>
 }
+
 
 export default MyApp

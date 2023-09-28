23b961d082d280440cd308e13b9933b77b5675c6
Shahanwaz <shahnawaz@alumeltech.com>
2023-09-28 11:08:29 +0530

    added code

--- utils/Loader.tsx
+++ utils/Loader.tsx
@@ -1,31 +1,14 @@
 import React, { useContext } from "react";
+import { ClipLoader } from "react-spinners";
 import { LoaderContext } from "../context/LoaderContext";
-
 const Loader: React.FC = () => {
     const { isLoading } = useContext(LoaderContext);
     return (
         <div
-            className={`fixed  top-0 left-0 right-0 bottom-0 overflow-hidden items-center justify-center z-[9999] ${isLoading ? 'flex' : 'hidden'}`}
-            style={{
-                backdropFilter: 'blur(50px) saturate(180%)',
-                WebkitBackdropFilter: 'blur(50px) saturate(180%)',
-                backgroundColor: 'rgba(255, 255, 255, 0.75)'
-            }}
-        >
-            <div id="container">
-                <div className="divider" aria-hidden="true"></div>
-                <p className="loading-text" aria-label="Loading">
-                    <span className="letter" aria-hidden="true">L</span>
-                    <span className="letter" aria-hidden="true">o</span>
-                    <span className="letter" aria-hidden="true">a</span>
-                    <span className="letter" aria-hidden="true">d</span>
-                    <span className="letter" aria-hidden="true">i</span>
-                    <span className="letter" aria-hidden="true">n</span>
-                    <span className="letter" aria-hidden="true">g</span>
-                </p>
-            </div>
+            className={`fixed top-0 left-0 right-0 bottom-0 items-center justify-center z-[9999] ${isLoading ? 'flex' : 'hidden'}`}
+            style={{ background: "rgba(0,0,0,.8)" }}>
+            <ClipLoader color={"#007FBB"} loading={isLoading} size={100} className="border-[4px]"/>
         </div>
     );
 };
-
 export default Loader;
